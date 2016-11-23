76dbef09f0d97cc2dc2c2d544660f45f5e3dd2bb
Trace Smith <tsmith5151@gmail.com>
2016-11-22 23:19:27 -0600

    final commit

--- DoingDataScience/Unit 11 Assignment/analysis.R
+++ DoingDataScience/Unit 11 Assignment/analysis.R
@@ -33,14 +33,21 @@ lines(fit$time.series[,2],col="red",ylab="Trend",lwd=2.5)
 ##e) Change one observation to be an outlier (e.g., add 500 to one observation), 
 #and recompute the seasonally adjusted data. What is the effect of the outlier?
 
-#add outlier to data
-#ts is used to create time-series objects
-hsales3 <- ts(c(hsales[1:275],hsales[5]+100), end=c(1995,11),
-              start=c(1973,1),frequency=15)
+#start year=1973, frequency= 12 months
+hsales2 <- ts(c(hsales[1:137],hsales[138]+500,hsales[139:275]),start=c(1973,1),frequency=12)
+plot(hsales2, col="purple",lwd=1.5, main="Sales of US One Family Housing\n(Outlier in Middle)",
+     ylab="New Single Family House Sales", xlab="Years")
 
-plot(hsales3,col="purple",lwd=1.5,
-     main="Modify 'hsales' Data with Outlier",
-     xlab = "Time", ylab="Monthly Sales")
+#Recompute the seasonally adjusted data
+ fit2 <- stl(hsales2, s.window=5)
+plot(fit2, col="brown",lwd=1.5, main="Seasonal Adjusted Data\n(Outlier in Middle)")
+
+#Plot of hsales2 with the seasonal adjusted data (green line)
+
+plot(hsales2, col="gray", lwd=1.5,
+     main="Sales of US Single Family Housing(Outlier in Middle)\nwith Seasonal Adjusted Data(green line)",
+     ylab="New Single Family House Sales", xlab="Years")
+lines(fit2$time.series[,2],col="green",lwd=1.5, ylab="Trend")
 
 ##f) Does it make any difference if the outlier is near the end rather than in the middle of 
 #the time series?
